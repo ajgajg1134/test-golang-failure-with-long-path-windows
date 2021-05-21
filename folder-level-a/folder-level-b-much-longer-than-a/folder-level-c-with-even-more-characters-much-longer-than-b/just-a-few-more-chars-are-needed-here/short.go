f87fe212f6135e6b796b2bb0f70622c936acfeaf
Andrew Glaude <aglaude@vistaprint.com>
2021-05-21 10:43:34 -0400

    test3

---
+++ folder-level-a/folder-level-b-much-longer-than-a/folder-level-c-with-even-more-characters-much-longer-than-b/just-a-few-more-chars-are-needed-here/short.go
@@ -0,0 +1,3 @@
+package just_a_few_more_chars_are_needed_here
+
+type TheBigOne struct{}
--- main_test.go
+++ main_test.go
@@ -1,16 +1,12 @@
 package main_test
 
 import (
-	once_more_into_the_breach "github.com/ajgajg1134/test-golang-failure-with-long-path-windows/folder-level-a/folder-level-b-much-longer-than-a/folder-level-c-with-even-more-characters-much-longer-than-b/folder_level_c_with_even_more_characters_and_further_more_a_longer_directory_name_much_longer_than_b/once-more-into-the-breach"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 //This tests seems to not be required to force vet to fail
 func TestMain_Potato_Test(t *testing.T) {
-	//_ = folder_level_c_with_even_more_characters_and_further_more_a_longer_directory_name_much_longer_than_b.Blah{}
-
-	_ = once_more_into_the_breach.BigBlah{}
 
 	assert.True(t, true)
 }
