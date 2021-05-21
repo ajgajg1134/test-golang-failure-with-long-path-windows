232c51cfbba3a41fdd04645e91bd6ab9491ec261
Andrew Glaude <aglaude@vistaprint.com>
2021-05-21 10:22:20 -0400

    try with test

--- main_test.go
+++ main_test.go
@@ -1,8 +1,13 @@
 package main_test
 
-// This tests seems to not be required to force vet to fail
-//func TestMain_Potato_Test(t *testing.T) {
-//	//_ = folder_level_c_with_even_more_characters_and_further_more_a_longer_directory_name_much_longer_than_b.Blah{}
-//
-//	assert.True(t, true)
-//}
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//This tests seems to not be required to force vet to fail
+func TestMain_Potato_Test(t *testing.T) {
+	//_ = folder_level_c_with_even_more_characters_and_further_more_a_longer_directory_name_much_longer_than_b.Blah{}
+
+	assert.True(t, true)
+}
